e18015477341f261b422d5d356b9d44dce206d3d
Pandi Durga <rpandidurga2002@gmail.com>
2024-04-03 18:35:22 +0530

    adding functionality changed

--- Add.java
+++ Add.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class Add{
     public static void main(String args[]){
         int a=100;
+        int b=90;
         System.out.println("hellow world");
-        System.out.println("this is a second change"+a);
+        System.out.println("this is a second change"+ (a+b));
     } 
 }
