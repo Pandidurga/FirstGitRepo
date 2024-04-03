fa472fbed604c6caae6a7de4452e6976018333fa
Pandidurga <110647877+Pandidurga@users.noreply.github.com>
2024-04-03 18:07:51 +0530

    Update Add.java a=0

--- Add.java
+++ Add.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class Add{
     public static void main(String args[]){
+        int a=0;
         System.out.println("hellow world");
-        System.out.println("this is a second change");
+        System.out.println("this is a second change"+a);
     } 
 }
