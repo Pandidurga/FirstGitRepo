85e9bd0d529571e7d6eb02ea178f8f8cf067cb67
Pandi Durga <rpandidurga2002@gmail.com>
2024-04-03 18:08:40 +0530

    a=10

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Add{
     public static void main(String args[]){
-        int a=0;
+        int a=10;
         System.out.println("hellow world");
         System.out.println("this is a second change"+a);
     } 
